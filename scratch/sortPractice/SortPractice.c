b1765119c5695184a8d48f1693c6603cea6f4469
ted-desktop-debian <zoogree@gmail.com>
2025-11-13 12:12:03 +0900

    주석 재정렬

--- scratch/sortPractice/SortPractice.c
+++ scratch/sortPractice/SortPractice.c
@@ -3,24 +3,22 @@
 #include <time.h>
 #include <string.h>
 
-// 작성된 정렬: 버블, 선택, 삽입, 쉘, 크누스수열 개선쉘, 퀵, 머지
-
 // Prototypes:
-// 정렬함수들이 상속하는 swap과 배열출력 편의를 위한 printArr
+
+// helper methods
 inline void swap(int* a, int* b);
 void printArr(int* arr, unsigned size);
-// basic sort
+
+// sort
 void bubble(int* arr, unsigned size);
 void select(int* arr, unsigned size);
 void insert(int* arr, unsigned size);
-// shell sort
 void shell(int* arr, unsigned size);
 void knuth_shell(int* arr, unsigned size);
-// quick sort
 void quickSort(int* arr, int left, int right);                          // quick 프로토타입
-void quick(int* arr, unsigned size) { quickSort(arr, 0, size - 1); }    // 테스트의 양식에 맞게 사용하기 위한 wrapper함수
-// merge
+void quick(int* arr, unsigned size) { quickSort(arr, 0, size - 1); }    // 테스트의 양식(매개변수 스타일)에 맞게 사용하기 위한 wrapper함수
 void merge(int* arr, unsigned size);                                    // 개념만 보고 직접 만든거라 스탠다드 코드와 약간 다름.
+
 // for sort time bench marking 
 int* getRandArr(long size, int maxInt);
 void doTest(void (*func)(int*, unsigned), unsigned loop, int* sampleArr, long size);
@@ -69,6 +67,7 @@ int main() {
 
 
 // 함수 구현부(정의)
+
 void swap(int* a, int* b) { register int tmp = *a;      *a = *b;        *b = tmp; }
 void printArr(int* arr, unsigned size) {
     printf("{%d", arr[0]);
@@ -77,7 +76,7 @@ void printArr(int* arr, unsigned size) {
     printf("}\n");
 }
 
-// 최소값 찾기지만, 큰 값을 찾아 배열 오른쪽으로 밀어냄. (거품이 교차되듯이)
+// 최소값 찾기. 큰 값을 찾아 배열 오른쪽으로 밀어냄 (거품이 교차되듯이)
 void bubble(int* arr, unsigned size) {
     for (int i = 0; i < size - 1; ++i) {
         for (int j = 0; j < size - 1 - i; ++j) {
@@ -88,7 +87,7 @@ void bubble(int* arr, unsigned size) {
     }
 }
 
-// 0번 인덱스부터 최소값을 찾고(선택), 있다면 0번과 스왑. 바뀐건 다음 루프에서 처리.
+// 0번 인덱스부터 최소값을 찾고(선택), 있다면 0번과 스왑. 바뀐건 다음 루프에서 처리
 void select(int* arr, unsigned size) {
     for (int i = 0; i < size; ++i) {
         int minIdx = i;                 // 인덱스로 저장하며 업데이트 후 , 최소값의 인덱스를 찾아 i와 minIdx를 스왑
@@ -102,18 +101,18 @@ void select(int* arr, unsigned size) {
 // i를 기준으로 i의 좌측 배열만 조정하며, 최소값/최대값 등을 저장후 삽입위치 정해질 때 까지 배열을 한칸씩 밀어냄.
 void insert(int* arr, unsigned size) {
     for (int i = 1; i < size; ++i) {    // 코드가 배열을 i기준으로 나누어 좌측, 우측배열을 비교하는 방식이므로, 1부터 시작.
-        int buffer = arr[i];           // 현재 요소 저장 (index로 사용하지 않는 이유는 j를 다루는 배열에서 이곳의 idx로 밀어낼 수 있음.)
+        int buffer = arr[i];            // 현재 요소 저장 (index로 사용하지 않는 이유는 j를 다루는 배열에서 이곳의 idx로 밀어낼 수 있음.)
         int j = i;
         // i 기준으로 좌우 배열 슬라이스. 이후 좌측배열의 마지막요소부터 0까지 점검후 밀어내기. 넣을곳이 있다면 삽입하고 루프종료
-        for (; j > 0 && arr[j - 1] > buffer; --j) 
-            arr[j] = arr[j - 1];
-        if (j != i) arr[j] = buffer;        // j(삽입할 위치)를 다루는 반복문이 j를 초기값i에서 더 작은 idx값으로 업데이트 했다면, 버퍼에 저장한 값으로 덮어씌움(삽입)
+        for (; j > 0 && arr[j - 1] > buffer; --j)       arr[j] = arr[j - 1];
+        if (j != i)         arr[j] = buffer;    // j(삽입할 위치)를 다루는 반복문이 j를 초기값i에서 더 작은 idx값으로 업데이트 했다면, 버퍼에 저장한 값으로 덮어씌움(삽입)
     }
 }
 
 // 쉘정렬은 insert의 확장개념. insert에서 int i = 1과 arr[j - 1]에 해당하는 부분이 gap(step)으로 관리되며 크기는 아무렇게나 줄여도 상관없으나, 마지막엔 항상 1을 실행해야 정확도를 보장함.
 // gap(step) : size / 2로 갱신할 것을 제안했으나, knuth수열(3^k - 1) / 2 (혹은 3new = 3old + 1) 혹은 치우라 실험검증 수열이 가장 빠르다고 알려짐. size / 2갱신도 충분히 빠름
-/* //!압도적으로 빠른 이유 분석: 
+
+/* 압도적으로 빠른 이유 분석: 
 1. 순수 삽입정렬은 매우 큰 이동이 필요한 경우(오름차순인데 마지막이 1처럼 극단적으로 작은 경우) 아주 잦은 이동이 필요함. 이것의 횟수를 증분활용을 통해 획기적으로 줄임.
 2. 삽입정렬은 애초에 어느정도 정렬되어 있어야 빠른데(데이터 이동 최소화), 증분의 단계중 이전 단계가 현재의 단계를 거의 완성시켜놓음.
 */ 
@@ -123,8 +122,7 @@ void shell(int* arr, unsigned size) {
         for (int i = step; i < size; ++i) {
             int buffer = arr[i];
             int j = i;
-            for (; j >= step && arr[j - step] > buffer; j -= step) 
-                arr[j] = arr[j - step];
+            for (; j >= step && arr[j - step] > buffer; j -= step)      arr[j] = arr[j - step];
             if (j != i) { arr[j] = buffer;}
         }
         step /= 2;
@@ -141,8 +139,7 @@ void knuth_shell(int* arr, unsigned size) {
     while (step > 0) { 
         for (i = step; i < size; ++i) {
             buffer = arr[i];
-            for (j = i; j >= step && arr[j - step] > buffer; j -= step) 
-                arr[j] = arr[j - step];
+            for (j = i; j >= step && arr[j - step] > buffer; j -= step)         arr[j] = arr[j - step];
             if (j != i) { arr[j] = buffer;}
         }
         step /= 3;                                      // update step
@@ -205,7 +202,7 @@ void merge(int* arr, unsigned size) {
 
 int* getRandArr(long size, int maxInt) {
     int* res = malloc(sizeof(int) * size);      // getRandArr메서드 사용시에는 메모리 해제를 잊지 않도록.
-    for (long i = 0; i < size; ++i)         res[i] = rand() % maxInt + 1;
+    for (long i = 0; i < size; ++i)             res[i] = rand() % maxInt + 1;
     return res;
 }
 
