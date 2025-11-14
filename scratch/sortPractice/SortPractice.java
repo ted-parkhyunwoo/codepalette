32dabd521566db89cf15e7b7ee78b7fb5d8c7d3d
ted-macbookAir <zoogree@gmail.com>
2025-11-15 01:19:05 +0900

    update SortPractice.java

--- scratch/sortPractice/SortPractice.java
+++ scratch/sortPractice/SortPractice.java
@@ -1,75 +1,175 @@
-public class SortPractice {
-    public static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+import java.util.ArrayList;
+
+class SortPractice {
+
+    // print arraylist helper
+    public static <T> void print(ArrayList<T> arr) {
+        System.out.print("[");
+        int sz = arr.size();
+        if (sz > 0) System.out.print(arr.get(0));
+        for (int i = 1; i < sz; ++i)
+            System.out.print(", " + arr.get(i));
+        System.out.println("]");
     }
 
-    public static void printArray(int[] arr) {
-        int size = arr.length;
-        if (size < 0) return;
-        System.out.print("{" + arr[0]);
-        for (int i = 1; i < size; ++i) {
-            System.out.print(", " + arr[i]);
-        }
-        System.out.println('}');
+    // swap helper
+    public static <T> void swap(ArrayList<T> arr, int i, int j) {
+        T tmp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, tmp);
     }
 
-    public static void bubble(int[] arr) {
-        int size = arr.length;
-        for (int i = 0; i < size - 1; ++i) {
-            for (int j = 0; j < size - i - 1; ++j) {
-                if (arr[j] > arr[j + 1]) {
+    // compare condition helper
+    public static <T extends Comparable<T>> boolean isNeedSwap(T a, T b, boolean ascending) {
+        return ascending? a.compareTo(b) > 0 : a.compareTo(b) < 0;
+    }
+
+
+    public static <T extends Comparable<T>> void bubble(ArrayList<T> arr, boolean ascending) {
+        int sz = arr.size();
+        for (int i = 0; i < sz - 1; ++i) {
+            for (int j = 0; j < sz - i - 1; ++j) {
+                if (isNeedSwap(arr.get(j), arr.get(j + 1), ascending)) 
                     swap(arr, j, j + 1);
-                }
             }
         }
     }
 
-    public static void select(int[] arr) {
-        int size = arr.length;
-        for (int i = 0; i < size; ++i) {
-            int minIndex = i;
-            for (int j = i; j < size; ++j) {
-                if (arr[j] < arr[minIndex])     minIndex = j;
+    public static <T extends Comparable<T>> void select(ArrayList<T> arr, boolean ascending) {
+        int sz = arr.size();
+        for (int i =0; i < sz; ++i) {
+            int minIdxBuffer = i;
+            for (int j = i; j < sz; ++j) {
+                if (isNeedSwap(arr.get(minIdxBuffer), arr.get(j), ascending))
+                    minIdxBuffer = j;
             }
-            if (minIndex != i)          swap(arr, i, minIndex);
+            if (minIdxBuffer != i)
+                swap(arr, i, minIdxBuffer);
         }
     }
 
-    public static void insert(int[] arr) {
-        int size = arr.length;
-        for (int i = 1; i < size; ++i) {
-            int buffer = arr[i];
-            int j = i;
-            while (j > 0 && arr[j - 1] > buffer) {
-                arr[j] = arr[j - 1];
-                --j;
-            }
-            arr[j] = buffer;
+    public static <T extends Comparable<T>> void insert(ArrayList<T> arr, boolean ascending) {
+        int sz = arr.size();
+        for (int i = 1; i < sz; ++i) {
+            T buffer = arr.get(i);
+            int j; 
+            for (j = i; j > 0 && isNeedSwap(arr.get(j - 1), buffer, ascending); --j)
+                arr.set(j, arr.get(j - 1));
+            if (j != i)
+                arr.set(j, buffer);
         }
     }
 
-    public static void shell(int[] arr) {
-        int size = arr.length;
-        for (int step = size / 2; step > 0; step /= 2) {
-            System.out.println("step: " + step);
-            for (int i = step; i < size; ++i) {
-                int buffer = arr[i];
-                int j = i;
-                for (; j >= step && arr[j - step] > buffer; j -= step) {
-                    arr[j] = arr[j - step];
-                }
-                if (j != i) arr[j] = buffer;
+    public static <T extends Comparable<T>> void shell(ArrayList<T> arr, boolean ascending) {
+        int sz = arr.size();
+        int step = 1;
+        while (step < sz / 3) step = step * 3 + 1;      // knuth gap.
+        while(step > 0) {
+            for (int i = step; i < sz; ++i) {
+                T buffer = arr.get(i);
+                int j;
+                for (j = i; j >= step && isNeedSwap(arr.get(j-step), buffer, ascending); j -= step)
+                    arr.set(j, arr.get(j - step));
+                if (j != i)
+                    arr.set(j, buffer);
             }
-
+            step /= 3;
         }
     }
+
+    public static <T extends Comparable<T>> void _quick(ArrayList<T> arr, int startIdx, int endIdx, boolean ascending) {
+        int lIdx = startIdx;
+        int rIdx = endIdx;
+        T pivot = arr.get((lIdx + rIdx) / 2);
+
+        while(lIdx <= rIdx) {
+            if (ascending) {
+                while(arr.get(lIdx).compareTo(pivot) < 0)       lIdx++;
+                while(arr.get(rIdx).compareTo(pivot) > 0)       rIdx--;
+            }
+            else {
+                while(arr.get(lIdx).compareTo(pivot) > 0)       lIdx++;
+                while(arr.get(rIdx).compareTo(pivot) < 0)       rIdx--;
+            }
+
+            if (lIdx <= rIdx) {
+                T buffer = arr.get(lIdx);
+                arr.set(lIdx, arr.get(rIdx));
+                arr.set(rIdx, buffer);
+                lIdx++;
+                if (rIdx > 0)       rIdx--;
+            }
+        }
+        
+        if (startIdx < rIdx)        _quick(arr, startIdx, rIdx, ascending);
+        if (endIdx > lIdx)          _quick(arr, lIdx, endIdx, ascending);
+    }
+
+    public static <T extends Comparable<T>> void quick(ArrayList<T> arr, boolean ascending) {
+        _quick(arr, 0, arr.size() - 1, ascending);
+    }
+
+    public static <T extends Comparable<T>> void _merge(ArrayList<T> arr, ArrayList<T> left, ArrayList<T> right, boolean ascending) {
+        int aSize = arr.size(), lSize = left.size(), rSize = right.size(), aIdx = 0, lIdx = 0, rIdx = 0;
+        while (aIdx < aSize && lIdx < lSize && rIdx < rSize) {
+            T leftBuffer = left.get(lIdx);
+            T rightBuffer = right.get(rIdx);
+            boolean cond = leftBuffer.compareTo(rightBuffer) < 0;
+            cond = ascending? cond : !cond;
+            if (cond)           arr.set(aIdx++, left.get(lIdx++));
+            else                arr.set(aIdx++, right.get(rIdx++));
+        }
+        while (lIdx < lSize)    arr.set(aIdx++, left.get(lIdx++));
+        while (rIdx < rSize)    arr.set(aIdx++, right.get(rIdx++));
+    }
+
+    public static <T extends Comparable<T>> void merge(ArrayList<T> arr, boolean ascending) {
+        int sz = arr.size();
+        if (sz <= 1)        return;
+        int lSize = sz / 2;
+        ArrayList<T> left = new ArrayList<>();
+        ArrayList<T> right = new ArrayList<>();
+        for (int i= 0; i < sz; ++i) {
+            if (i < lSize)      left.add(arr.get(i));
+            else                right.add(arr.get(i));   
+        }
+
+        merge(left, ascending);
+        merge(right, ascending);
+        _merge(arr, left, right, ascending);
+    }
+
+
+    // init sample arraylist helper.
+    public static void init(ArrayList<Integer> arr) {
+        arr.clear();
+        int[] sample = {5,2,9,3,1,4,8,0,6,7};
+        for (int i = 0; i < sample.length; ++i)
+            arr.add((Integer)sample[i]);
+    }
+    public static void initStrList(ArrayList<String> arr) {
+        arr.clear();
+        String[] strarr = {"c", "b", "A", "3", "5", "ab", "abc", "Ca", "aB", "Ab"};
+        for (int i = 0; i < strarr.length; ++i)
+            arr.add((String)strarr[i]);
+    }
+
+
 
     public static void main(String[] args) {
-        int[] test = new int[] { 2, 3, 7, 1, 9, 6, 0, 5, 4, 8 };
-        printArray(test);
-        shell(test);
-        printArray(test);
+        ArrayList<Integer> test = new ArrayList<>();
+        boolean ascending = false;
+        init(test); bubble(test, ascending); print(test);
+        init(test); select(test, ascending); print(test);
+        init(test); insert(test, ascending); print(test);
+        init(test); shell(test, ascending); print(test);
+        init(test); quick(test, ascending); print(test);
+        init(test); merge(test, ascending); print(test);
+        
+        ArrayList<String> strTest = new ArrayList<>();
+        initStrList(strTest);
+        merge(strTest, true);
+        print(strTest);
+        
     }
 }
