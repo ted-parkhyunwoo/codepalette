e81c082166e780701d2ded18c0bd131feea65c47
ted-desktop-debian <zoogree@gmail.com>
2025-12-03 00:18:01 +0900

    refactor: 주석개선 및 바이너리 출력문 개선

--- scratch/binarySwitch.c
+++ scratch/binarySwitch.c
@@ -17,7 +17,8 @@ void print_uint8_binary(uint8_t n) {
     printf("\n"); // 출력 후 줄 바꿈
 }
 
-void printBin(uint8_t n) {
+// 직접구현(메모리 할당/해제 오버헤드 있음)
+void printBin2(uint8_t n) {
     char* binString = malloc(sizeof(char) * 9);    // 반전 위해 필요한 공간.
     
     unsigned binIdx = 0;
@@ -26,52 +27,62 @@ void printBin(uint8_t n) {
         n >>= 1;
     }
     for (; binIdx < 8; binIdx++)        binString[binIdx] = '0';
-    binString[binIdx] = '\0';
+    binString[8] = '\0';        // 올바른 free를 위한 널문자 추가.
 
-    for (unsigned i = 7; i > 0; i--)   printf("%c", binString[i]);
-    printf("%c\n", binString[0]);
+    // unsigned에 0보다 작아지는 순간 발생할 수 있는 문제 제어 추가된 비트출력
+    for (unsigned i = 7; i > 0; i--)    printf("%c", binString[i]);
+    printf("%c\n", binString[0]);   
 
     free(binString);
 }
+
+// printBin2 개선을 위해 직접구현 했으나 gemini와 별반 다를게 없어짐.
+void printBin(uint8_t n) {
+    for (int i = 7; i >= 0; --i)
+        // n & (1 << i) 는 정수이며, 결과가 0과 1이 아니라 실제로는 자릿수별 정수값(1, 2, 4, 8...)이 값이 있냐 없냐(0)로 boolean 처리됨에 유의.
+        printf("%c", (n & (1 << i))? '1': '0');     
+    printf("\n");
+}
+
 
 int main() {
     uint8_t tmp = 12;
 
     // 진수표기별 변수 저장
-    tmp = 0b1100;   // 2진수 표현(12)
-    tmp = 014;      // 8진수 표현(12)
-    tmp = 0x000c;   // 16진수표현(아래 모두 12로 같은값)
+    tmp = 0b1100;           // 2진수 표현(12)
+    tmp = 014;              // 8진수 표현(12)
+    tmp = 0x000c;           // 16진수표현(아래 모두 12로 같은값)
     tmp = 0xC;
     tmp = 0x0c;
     
     // 진수 출력
-    printf("진수출력:\n");
+    printf("\n진수출력:\n");
 
     // 안타깝게도 print format은 2진수 출력 안됨. 8, 16진수는 가능
     printf("10:%d, 8:%o, 16:%x, 16:%X\n", tmp, tmp, tmp, tmp);
-    print_uint8_binary(12);     // gemini 구현
-    printBin(12);               // 직접 구현 
+    print_uint8_binary(12);         // gemini 구현
+    printBin(12);                   // 직접 구현 
 
 
     // 논리연산자
-    printf("논리연산: \n");
+    printf("\n논리연산: \n");
 
     // 논리곱(AND), 논리합(OR), 반전(!기호와 다름. NOT이란 명칭은 같음)
-    print_uint8_binary(~0b1010);            // 11110101
-    print_uint8_binary(0b1111 & 0b0101);    // 1111 & 0101 -> 0101
-    print_uint8_binary(0b0101 | 0b1010);    // 0101 | 1010 -> 1111
+    printBin(~0b1010);              // 11110101
+    printBin(0b1111 & 0b0101);      // 1111 & 0101 -> 0101
+    printBin(0b0101 | 0b1010);      // 0101 | 1010 -> 1111
     
     // 복합(NAND: NOT AND ~(A & B) , NOR: NOT OR ~(A | B)) 이 있음.
 
     // bit shift.
-    printf("비트시프트:\n");
-    print_uint8_binary(0b1010 >> 1);        // 0101
-    print_uint8_binary(0b0001 << 1);        // 0010
+    printf("\n비트시프트:\n");
+    printBin(0b1010 >> 1);          // 0101
+    printBin(0b0001 << 1);          // 0010
     // 특징: bitshift 진행시마다 값은 2배 커지거나 2배 나누어짐(나머지 버림 처리)
 
 
     // 스위치 제어 예제. mem을 손상하지 않고 res로 업데이트
-    printf("논리연산자를 통한 스위치 제어 예제:\n");
+    printf("\n논리연산자를 통한 스위치 제어 예제:\n");
     uint8_t mem, mask, res;
     
     mem =  0B00000011;
@@ -79,21 +90,21 @@ int main() {
 
     // mem에서 mask로 지정된 것이 활성화 되어있는지 검사
     res =  mem & mask;
-    printf("활성스위치: ");
-    print_uint8_binary(res);        // 00000001
+    printf("활성스위치: \t");
+    printBin(res);                  // 00000011 & 00000001      ->  00000001
 
     mem =  0B01010101;
     mask = 0B10101010;
 
     // 켜기(전에 켜져있든 꺼져있든)
-    res = mem | mask;
-    printf("켜기 결과: ");
-    print_uint8_binary(res);        // 11111111
-    
+    res = mem | mask;               // 01010101 | 10101010      ->  11111111
+    printf("켜기 결과: \t");
+    printBin(res);
+
     // 끄기(A & ~B)
-    res = res & ~mask;
-    printf("끄기 결과: ");
-    print_uint8_binary(res);        // 01010101;
+    res &= ~mask;                   // 11111111 & 01010101(~10101010)   ->  01010101    
+    printf("끄기 결과: \t");
+    printBin(res);
 
 
 	return 0;
