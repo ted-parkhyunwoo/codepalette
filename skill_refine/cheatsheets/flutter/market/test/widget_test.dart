bbe2cd479e50960141a1d15efe96eb32e0c70e78
ted-desktop-debian <zoogree@gmail.com>
2025-12-12 00:00:06 +0900

    fix: 앱 이름 변경

--- skill_refine/cheatsheets/flutter/market/test/widget_test.dart
+++ skill_refine/cheatsheets/flutter/market/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:contact/main.dart';
+import 'package:market/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
