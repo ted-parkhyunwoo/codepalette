adf9ed6b7d1e5d444695dd8586e1f5f068499b74
ted-desktop-debian <zoogree@gmail.com>
2025-12-12 21:31:23 +0900

    add: flutter basic

---
+++ skill_refine/current_courses/flutter/flutter_projects/basics/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(),
+          child: Center(child: Text("Hello World"))),
+      ),
+    );
+  }
+}
