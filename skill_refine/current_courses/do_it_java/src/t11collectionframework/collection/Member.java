06b8b532fd87384a77228ed19b7783931b97b404
ted-windows-desktop <zoogree@gmail.com>
2025-12-22 04:25:02 +0900

    fix: 주석 구체화, TreeSet을 위한 Comparable, compateTo 구현

--- skill_refine/current_courses/do_it_java/src/t11collectionframework/collection/Member.java
+++ skill_refine/current_courses/do_it_java/src/t11collectionframework/collection/Member.java
@@ -2,7 +2,8 @@ package t11collectionframework.collection;
 
 // 해당 Member DataClass를 이용해 콜렉션별 추가삭제 기능과 테스트가 작성되었다.
 
-public class Member {
+public class Member implements Comparable<Member> {
+    //! TreeSet을 위해 implements Comparable<Member> 추가(compareTo도 구현해야함)
     static int idBuffer = 1000;
     private int memberId;
     private String memberName;
@@ -12,7 +13,7 @@ public class Member {
         this.memberId = idBuffer++;
     }
 
-    // memberId를 강제로 추가(set 등 테스트를 위한)
+    //! memberId를 강제로 추가(set 등 테스트를 위한)
     public Member(int memberId, String memberName) {
         this.memberId = memberId;
         this.memberName = memberName;
@@ -21,6 +22,7 @@ public class Member {
     public int getMemberId() {
         return memberId;
     }
+
     public String getMemberName() {
         return memberName;
     }
@@ -31,7 +33,8 @@ public class Member {
     }
 
 
-    // hashSet을 사용하기 위해 memberId기반 hashCode, equals를 오버라이드 (memberId와 hashCode 가 같으면 같은객체라 판단하도록 유도)
+    //! hashSet을 사용하기 위해 memberId기반 hashCode, equals를 오버라이드 (memberId와 hashCode 가 같으면 같은객체라 판단하도록 유도)
+    // hashCode가 같은지 먼저 살피고 최종 기준은 equals를 검사하는 구조라 함
     @Override
     public int hashCode() {
         return memberId;
@@ -44,5 +47,12 @@ public class Member {
             return bf.getMemberId() == this.getMemberId();
         }
         return false;
+    }
+
+    //! TreeSet은 이진탐색 트리기반 정렬을 하여 요소중복여부 판단, 요소의 위치를 정렬할 기준의 함수를 만들어줘야함.
+    @Override
+    public int compareTo(Member member) {
+        // 오름차순으로 정렬. * -1 하면 내림차순
+        return (this.memberId - member.memberId);
     }
 }
