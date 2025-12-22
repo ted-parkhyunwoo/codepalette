ead050c84415e4f622c030a56b3398322a9928e1
ted-desktop-debian <zoogree@gmail.com>
2025-12-23 01:07:09 +0900

    add: 이너클래스 작성중

---
+++ skill_refine/current_courses/do_it_java/src/t12innerlambdastream/innerclass/Test.java
@@ -0,0 +1,17 @@
+package t12innerlambdastream.innerclass;
+
+public class Test {
+    public class Test2{
+
+    }
+    public static void main(String[] args) {
+        class Test3{
+
+        }
+        Test3 t;
+
+    }
+}
+
+
+//! 작성 보류.
