fcb076ce6cb6da6744e6f9c0ae7289d023a310ea
ted-windows-desktop <zoogree@gmail.com>
2026-01-02 03:23:05 +0900

    fix: 오토클로저블 개념을 소멸자나 dispose()같은 gc 힌트 메서드로 인식될 수 있도록 주석 개선

--- skill_refine/current_courses/do_it_java/src/t13exception/AutoCloseObj.java
+++ skill_refine/current_courses/do_it_java/src/t13exception/AutoCloseObj.java
@@ -1,9 +1,14 @@
 package t13exception;
 
+
+//! cpp의 소멸자, 거의 비슷한건 dart 의 dispose() 처럼, close를 override하여 사용
+// finalize() 라는 소멸자개념이 있었으나, 성능/안정성 문제로 폐기수순이며 거의 금지된 방법
+
 public class AutoCloseObj implements AutoCloseable{
     @Override
-    // 또 다른 예외는 throws함.
+    // 이외 발생할지도 모르는 또 다른 예외는 throws
     public void close() throws Exception {
+        // 이곳에 자원 회수를 위한 코드를 적음
         System.out.println("Auto Close obj");
     }
 }
